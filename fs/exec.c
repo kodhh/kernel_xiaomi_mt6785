e9bcc96dc741603fe5a4862c0d39c41d56e09a94
kodhh <limw94@outlook.com>
2024-06-05 18:24:35 +0800

    Add fpsgo_cus udc

--- fs/exec.c
+++ fs/exec.c
@@ -1707,8 +1707,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #if defined(CONFIG_KSU) && !(defined(CONFIG_KPROBES))
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
 #endif
 /*
  * sys_execve() executes a new program.
@@ -1725,7 +1728,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 
 #if defined(CONFIG_KSU) && !(defined(CONFIG_KPROBES))
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 #endif
 
 	if (IS_ERR(filename))
