c868224f716b951f7d2183cad6de677b7af71249
kodhh <limw94@outlook.com>
2024-06-05 21:52:27 +0800

    clean

--- fs/exec.c
+++ fs/exec.c
@@ -1707,11 +1707,8 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #if defined(CONFIG_KSU) && !(defined(CONFIG_KPROBES))
-extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
 #endif
 /*
  * sys_execve() executes a new program.
@@ -1728,10 +1725,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 
 #if defined(CONFIG_KSU) && !(defined(CONFIG_KPROBES))
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 #endif
 
 	if (IS_ERR(filename))
